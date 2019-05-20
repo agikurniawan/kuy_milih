a1b79eee45bd41d80a12152a6d7f4668ac46258d
unknown <naufalsetyanto39@gmail.com>
2019-05-20 20:33:54 +0700

    add pemilih

---
+++ pemilih/milih.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="../assets/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="../assets/Ionicons/css/ionicons.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+      <div class="row">
+      <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="">LOGO</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <!--ACCESS MENUS FOR ADMIN-->
+                  <li class="active"><a href="#">Dashboard</a></li>
+                  <li><a href="">login</a></li>
+                  <li><a href="">register</a></li>
+                
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Assalamualaikum</a></li>
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div><!--/.container-fluid -->
+        </nav>
+ 
+      </div>
+    <center><h2>KANDIDAT CALON RW ASGARD</h2></center>
+    <div class="row" style="margin-bottom:150px;">
+        <div class="col-md-6">
+            <center><h3>CALON 01</h3>
+                <img src="../assets/img/default.png" class="img-responsive img-thumbnail" />
+                <p><h5><strong>NAMA CALON RW</strong></h5></p>
+                <p>
+               <a href="../pemilih/visimisi.php" class="btn btn-primary" >Lihat Visi Misi <i class="glyphicon glyphicon-play"></i></a>
+                </p>
+                <p>
+               <a href="#" class="btn btn-danger" >Beri Suara<i class="glyphicon glyphicon-play"></i></a>
+                </p>
+            </center>
+            
+        </div>
+
+        <div class="col-md-6">
+             <center><h3>CALON 02</h3>
+                <img src="../assets/img/default.png" class="img-responsive img-thumbnail" />
+                <p><h5><strong>NAMA CALON RW</strong></h5></p>
+                <p>
+               <a href="#" class="btn btn-primary" >Lihat Visi Misi <i class="glyphicon glyphicon-play"></i></a>
+                </p>
+                <p>
+               <a href="#" class="btn btn-danger" >Beri Suara<i class="glyphicon glyphicon-play"></i></a>
+                </p>
+            </center>
+        </div>
+      </div>
+
+    
+
+        <center><h2>KANDIDAT CALON RT ASGARD</h2></center>
+    <div class="row">
+        <div class="col-md-6">
+            <center><h3>CALON 01</h3>
+                <img src="../assets/img/default.png" class="img-responsive img-thumbnail" />
+                <p><h5><strong>NAMA CALON RT</strong></h5></p>
+                <p>
+               <a href="#" class="btn btn-primary" >Lihat Visi Misi <i class="glyphicon glyphicon-play"></i></a>
+                </p>
+                <p>
+               <a href="#" class="btn btn-danger" >Beri Suara<i class="glyphicon glyphicon-play"></i></a>
+                </p>
+            </center>
+            
+        </div>
+
+        <div class="col-md-6">
+             <center><h3>CALON 02</h3>
+                <img src="../assets/img/default.png" class="img-responsive img-thumbnail" />
+                <p><h5><strong>NAMA CALON RT</strong></h5></p>
+                <p>
+               <a href="#" class="btn btn-primary" >Lihat Visi Misi <i class="glyphicon glyphicon-play"></i></a>
+                </p>
+                <p>
+               <a href="#" class="btn btn-danger" >Beri Suara<i class="glyphicon glyphicon-play"></i></a>
+                </p>
+            </center>
+        </div>
+      </div>
+
+    </div>
+
+    
+
+ 
+    <script src="../assets/js/bootstrap.min.js"></script>
+  </body>
+</body>
+</html>
